673e6863f85d75d8c52ec2a36e7a8d13cb08de16
Glêsio Santos <glesio.s.silva@gmail.com>
2022-10-29 11:51:21 -0300

    test: ensure SignUpController return 400 when password confirmation is fails

--- src/presentation/controllers/signup_controller.spec.ts
+++ src/presentation/controllers/signup_controller.spec.ts
@@ -83,6 +83,21 @@ describe('SignUp Controller ', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 
+  it('should return 400 when no password Confirmation is fails', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com.br',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('password'))
+  })
+
   it('should return 400 when an invalid email is provided', () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
