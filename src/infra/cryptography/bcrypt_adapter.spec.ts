9f44a032c70648a5e3a054ef2d1d5fa3ca558e0a
Glêsio Santos <glesio.s.silva@gmail.com>
2022-10-31 18:31:07 -0300

    test: ensure BcryptAdapter throw when bcrypt throws

--- src/infra/cryptography/bcrypt_adapter.spec.ts
+++ src/infra/cryptography/bcrypt_adapter.spec.ts
@@ -2,8 +2,8 @@ import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt_adapter'
 
 jest.mock('bcrypt', () => ({
-  hash: (): string => {
-    return 'hashed_password'
+  hash: async (): Promise<string> => {
+    return new Promise(resolve => resolve('hashed_password'))
   }
 }))
 
@@ -19,5 +19,12 @@ describe('Bcrypt Adapter', () => {
     const sut = new BcryptAdapter()
     const hashed = await sut.encrypt('valid_password')
     expect(hashed).toBe('hashed_password')
+  })
+
+  it('should throw when bcrypt throws', async () => {
+    const sut = new BcryptAdapter()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('valid_password')
+    await expect(promise).rejects.toThrow()
   })
 })
