5ee14cb4da1a5b4ecb749a7a30516fca8158ad39
Glêsio Santos <glesio.s.silva@gmail.com>
2022-11-02 08:19:48 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/data/protocols/add_account_repository.ts
+++ src/data/protocols/add_account_repository.ts
@@ -1,5 +1,5 @@
 import { AccountModel, AddAccountModel } from '../usecase/add-account/db_add_account_protocols'
 
 export interface AddAccountRepository {
-  add(account: AddAccountModel): Promise<AccountModel>
+  add(accountData: AddAccountModel): Promise<AccountModel>
 }
