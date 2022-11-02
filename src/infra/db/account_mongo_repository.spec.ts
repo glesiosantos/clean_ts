05486beadd85a78cd5059a4747c26f53d8e8c0c2
Glêsio Santos <glesio.s.silva@gmail.com>
2022-11-02 08:23:38 -0300

    test: add beforeEach to clear database

--- src/infra/db/account_mongo_repository.spec.ts
+++ src/infra/db/account_mongo_repository.spec.ts
@@ -8,6 +8,11 @@ describe('Account Mongo Repository', () => {
 
   afterAll(async () => await MongoHelper.disconnect())
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   it('should return an account on success', async () => {
     const sut = makeSut()
     const account = await sut.add({
