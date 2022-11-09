bdd40f9e3f1843702b969d2cb96fa8a125b70e9c
Glêsio Santos <glesioss@gmail.com>
2022-11-09 14:31:44 -0300

    feat: ensure LogErrorControllerDecorator return the same result of the controller

--- src/main/decorators/log_error_controller_decorator.ts
+++ src/main/decorators/log_error_controller_decorator.ts
@@ -3,7 +3,7 @@ import { Controller, HttpRequest, HttpResponse } from '../../presentation/protoc
 export class LogErrorControllerDecorator implements Controller {
   constructor(private readonly controller: Controller) { }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.controller.handle(httpRequest)
-    return new Promise(resolve => resolve(null))
+    const httpResponse = await this.controller.handle(httpRequest)
+    return httpResponse
   }
 }
