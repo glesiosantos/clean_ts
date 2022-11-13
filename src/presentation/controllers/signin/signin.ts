62f31e31fcbcf4f681bd8e9b5607eb6669d93d07
Glêsio Santos <glesioss@gmail.com>
2022-11-13 09:18:42 -0300

    feat: ensure SignInController return badRequest when EmailValidator return false

--- src/presentation/controllers/signin/signin.ts
+++ src/presentation/controllers/signin/signin.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http_helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup_protocols'
@@ -12,6 +12,10 @@ export class SignInController implements Controller {
         return new Promise(resolve => resolve(badRequest(new MissingParamError(field))))
       }
     }
-    this.emailValidator.isValid(httpRequest.body.email)
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValid) {
+      return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+    }
   }
 }
