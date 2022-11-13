d25d3af5be4b831f6652a7ec089fe90845f01063
Glêsio Santos <glesioss@gmail.com>
2022-11-13 08:21:26 -0300

    test: ensure SignInController return badRequest when no password is provided

--- src/presentation/controllers/signin/signin.spec.ts
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -15,4 +15,15 @@ describe('Sign In Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
+
+  it('should return badRequest when no password is provided ', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
 })
