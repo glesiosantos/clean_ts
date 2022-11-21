684e45a6fcbde675ed6659b115545bcb0537bbe5
Glêsio Santos <glesioss@gmail.com>
2022-11-21 10:07:38 -0300

    test: ensure ValidationComposite not return error when validation succeeds

--- src/presentation/helpers/validators/validation_composite.spec.ts
+++ src/presentation/helpers/validators/validation_composite.spec.ts
@@ -38,4 +38,10 @@ describe('Vaidation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
+
+  it('should not return when validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
