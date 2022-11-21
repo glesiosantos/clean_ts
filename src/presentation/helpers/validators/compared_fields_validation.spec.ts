8abf8a9d836cd7571ce36e29a5888892e5238335
Glêsio Santos <glesioss@gmail.com>
2022-11-21 09:46:22 -0300

    test: ensure ComparedFieldValidation return a invalidParamError when validation fails

--- src/presentation/helpers/validators/compared_fields_validation.spec.ts
+++ src/presentation/helpers/validators/compared_fields_validation.spec.ts
@@ -2,27 +2,22 @@ import { InvalidParamError } from '../../errors'
 import { ComparedFieldValidation } from './compared_fields_validation'
 import { Validation } from './validation'
 
-type SutTypes = {
-  sut: Validation
-}
+const makeSut = (): Validation => {
+  const sut = new ComparedFieldValidation('field', 'fieldCompared')
 
-const makeSut = (): SutTypes => {
-  const sut = new ComparedFieldValidation('password', 'passwordConfirmation')
-
-  return { sut }
+  return sut
 }
 
 describe('Compared Fields Validaton', () => {
   it('should return an error when password confirmation fails', () => {
-    const { sut } = makeSut()
-    jest.spyOn(sut, 'validate').mockReturnValueOnce(new InvalidParamError('password'))
-    const httpResponse = sut.validate('password')
-    expect(httpResponse).toEqual(new InvalidParamError('password'))
+    const sut = makeSut()
+    const result = sut.validate({ field: 'any_value', fieldCompared: 'wrong_value' })
+    expect(result).toEqual(new InvalidParamError('fieldCompared'))
   })
 
   it('should return null when password confirmation on success', () => {
-    const { sut } = makeSut()
-    const httpResponse = sut.validate('password')
-    expect(httpResponse).toBeFalsy()
+    const sut = makeSut()
+    const result = sut.validate({ field: 'any_value', fieldCompared: 'any_value' })
+    expect(result).toBeFalsy()
   })
 })
