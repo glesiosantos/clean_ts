b0c3a83da22e85101fcb2756cee361c8790a80e9
Glêsio Santos <glesioss@gmail.com>
2022-11-29 08:34:44 -0300

    feat: ensure AccountMongoRepository update the account on updateAccessToken success

--- src/infra/db/account_mongo_repository.ts
+++ src/infra/db/account_mongo_repository.ts
@@ -1,10 +1,17 @@
+import { ObjectId } from 'mongodb'
 import { AddAccountRepository } from '../../data/protocols/db/add_account_repository'
 import { LoadAccountByEmailRepository } from '../../data/protocols/db/load_account_by_email_repository'
+import { UpdateAccessTokenRepository } from '../../data/protocols/db/update_access_token_repository'
 import { AccountModel } from '../../domain/models/account_model'
 import { AddAccountModel } from '../../domain/usecase/add_account'
 import { MongoHelper } from './helpers/mongo_helper'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({ _id: new ObjectId(id) }, { $set: { accessToken: token } })
+  }
+
   async loadByEmail(email: string): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.findOne({ email })
