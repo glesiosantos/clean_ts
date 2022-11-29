9f14a87f44e3bd9105aef63429856c26d5f9745a
Glêsio Santos <glesioss@gmail.com>
2022-11-29 07:13:48 -0300

    test: ensure JwtAdapter throws when sign throws

--- src/infra/cryptography/jwt_adapter.spec.ts
+++ src/infra/cryptography/jwt_adapter.spec.ts
@@ -18,4 +18,11 @@ describe('Jwt Adapter', () => {
     const accessToken = await sut.generate('any_id')
     expect(accessToken).toBe('any_token')
   })
+
+  it('should throws when sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.generate('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
