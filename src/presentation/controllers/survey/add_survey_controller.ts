40f7391df9db5659c927a4a0f64d25befff35531
Glêsio Santos <glesioss@gmail.com>
2022-12-10 08:08:02 -0300

    feat: ensure AddSurveyController return serverError when AddSurvey throws

--- src/presentation/controllers/survey/add_survey_controller.ts
+++ src/presentation/controllers/survey/add_survey_controller.ts
@@ -1,12 +1,16 @@
 import { HttpRequest, HttpResponse } from '../../protocols'
-import { AddSurvey, badRequest, Controller, Validation } from './survey_protocols'
+import { AddSurvey, badRequest, Controller, serverError, Validation } from './survey_protocols'
 
 export class AddSurveyController implements Controller {
   constructor(private readonly validation: Validation, private readonly addSurvey: AddSurvey) { }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) return badRequest(error)
-    await this.addSurvey.add(httpRequest.body)
-    return null
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) return badRequest(error)
+      await this.addSurvey.add(httpRequest.body)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
