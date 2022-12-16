a70bcffc197ca4c806b8c99d52e7e40d831ebe78
Glêsio Santos <glesioss@gmail.com>
2022-12-16 10:43:24 -0300

    test: ensure AuthMiddleware return forbiden when LoadAccountByToken return null

--- src/presentation/middlewares/auth_middleware.spec.ts
+++ src/presentation/middlewares/auth_middleware.spec.ts
@@ -48,4 +48,11 @@ describe('Auth Middlewares', () => {
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
+
+  it('should return 403 when LoadAccountByToken returns nulls', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeneidError()))
+  })
 })
