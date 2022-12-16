49a8f152bd4fc4cc195b575e40b3387af661cbd6
Glêsio Santos <glesioss@gmail.com>
2022-12-16 11:20:38 -0300

    test: ensure AuthMiddleware return 500 when LoadAccountByToken throws

--- src/presentation/middlewares/auth_middleware.spec.ts
+++ src/presentation/middlewares/auth_middleware.spec.ts
@@ -1,5 +1,5 @@
 import { AccessDeneidError } from '../errors'
-import { forbidden, ok } from '../helpers/http/http_helper'
+import { forbidden, ok, serverError } from '../helpers/http/http_helper'
 import { HttpRequest } from '../protocols'
 import { AuthMiddlewares } from './auth_middleware'
 import { LoadAccountByToken } from '../../domain/usecase/load_account_by_token'
@@ -54,6 +54,13 @@ describe('Auth Middlewares', () => {
     jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve(null)))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(forbidden(new AccessDeneidError()))
+  })
+
+  it('should return 500 when LoadAccountByToken throws', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 
   it('should calls LoadAccountByToken with correct values', async () => {
